00292498bd30a3c6bb20426a2c48767ee3be68f8
kovachhh <koval.viktor338@gmail.com>
2024-09-27 21:28:50 +0300

    returned aligning user board

--- src/app/components/UserBoard/index.tsx
+++ src/app/components/UserBoard/index.tsx
@@ -12,7 +12,7 @@ type UserBoardProps = {
 const UserBoard: React.FC<UserBoardProps> = ({ isLoading, filteredUsers }) => {
   return (
     <div style={{ padding: '24px' }}>
-      <Row gutter={[64, 32]} justify='start'>
+      <Row gutter={[64, 32]} justify='center'>
         {!!filteredUsers.length &&
           filteredUsers.map((item) => (
             <Col xs={24} sm={14} md={10} lg={6} key={item._id}>
